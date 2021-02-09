a84d2fc076afcca458f50b34644e70ef2a3e70c1
brenosekiro <b88660501@gmail.com>
2021-02-08 22:40:20 -0300

    bug

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -34,7 +34,7 @@ class ContactHelper {
 
     return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
       await db.execute(
-        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT"
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
           "$phoneColumn TEXT, $imgColumn TEXT)"
       );
     });
